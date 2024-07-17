1de1f8a15580e16e603c1ea25298f39b440667d9
Katarzyna Ciurkot <kat99@onet.pl>
2024-07-17 12:21:28 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// console.log("hello world");
+// anime({
+//     targets: '.img-hero',
+//     scale: {
+//         value: 2,
+//         duration: 1600,
+//         delay: 800,
+//         easing: 'easeInOutQuart',
+//     }
+//     delay: 250 // All properties except 'scale' inherit 250m delay
+//   });
+  
